cdcee046cbdc0b7d147e4eebe762cde6387bbdb7
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-25 15:26:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your code in this file!
-
+let currentUser = "nayna"
+let welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + currentUser.toUpperCase() + "!"
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!"
